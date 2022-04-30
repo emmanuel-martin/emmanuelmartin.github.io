3a431c13b320d2bffa9d9de18f95ebd511e3d14a
Emmanuel Martin <helloiamemmanuel@gmail.com>
2022-04-30 22:24:52 +0530

    update

--- form/contact.php
+++ form/contact.php
@@ -1,32 +1,20 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
 
-// an email address that will be in the From field of the email.
+
 $from = 'Emmanuel Martin <helloiamemmanuel@gmail.com>';
 
-// an email address that will receive the email with the output of the form
 $sendTo = 'Emmanuel Martin <helloiamemmanuel@gmail.com>';
 
-// subject of the email
-$subject = 'New message from your website';
+$subject = 'Message from my website';
 
-// form field names and their translations.
-// array variable name => Text to appear in the email
 $fields = array('InputName' => 'Name', 'InputEmail' => 'Email', 'InputSubject' => 'Subject', 'InputMessage' => 'Message'); 
 
-// message that will be displayed when everything is OK :)
 $okMessage = 'Thank you for the message, I will get back to you soon!';
 
-// If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-/*
- *  LET'S DO THE SENDING
- */
 
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+
 error_reporting(E_ALL & ~E_NOTICE);
 
 try
@@ -34,16 +22,14 @@ try
 
     if(count($_POST) == 0) throw new \Exception('Form is empty');
             
-    $emailText = "You have a new message from your contact form\n=============================\n";
+    $emailText = "Someone has send you a message \n=============================\n";
 
     foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
         if (isset($fields[$key])) {
             $emailText .= "$fields[$key]: $value\n";
         }
     }
 
-    // All the neccessary headers for the email.
     $headers = array('Content-Type: text/plain; charset="UTF-8";',
         'From: ' . $from,
         'Reply-To: ' . $from,
@@ -61,7 +47,6 @@ catch (\Exception $e)
 }
 
 
-// if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
 
@@ -69,7 +54,6 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 
     echo $encoded;
 }
-// else just display the message
 else {
     echo $responseArray['message'];
 }
